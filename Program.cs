434f0a717acf75428ac692c8aaf8a3859ac37dd0
Elena Ghitescu <eghitescu@yahoo.com>
2018-08-23 13:01:56 +0200

    début

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NoSpoon1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
